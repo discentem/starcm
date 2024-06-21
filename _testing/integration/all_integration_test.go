4ba59557ded5e5abc090727c8f57c67c4b232110
BK <kurtz.brandon@gmail.com>
2024-06-20 23:16:06 -0700

    add integration test and integration test runner

---
+++ _testing/integration/all_integration_test.go
@@ -0,0 +1,44 @@
+package integrationtest
+
+import (
+	"flag"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAll(t *testing.T) {
+	testDir := flag.String("test_dir", "../../examples", "Path to the directory containing the test files")
+	starcm := flag.String("path_to_starcm_main.go", "../../main.go", "Path to the main.go file of the StarCM project")
+
+	err := filepath.Walk(*testDir, func(path string, info os.FileInfo, err error) error {
+		if strings.HasSuffix(path, ".expect") {
+			t.Run(path, func(t *testing.T) {
+				cmd := exec.Command(
+					"go",
+					"run",
+					*starcm,
+					"--root_file",
+					strings.TrimSuffix(path, ".expect"),
+					"--timestamps=false",
+				)
+				t.Log("Running starcm with", path)
+				actual, err := cmd.CombinedOutput()
+				assert.NoError(t, err)
+
+				expected, err := ioutil.ReadFile(path)
+				assert.NoError(t, err)
+
+				assert.Equal(t, string(expected), string(actual))
+			})
+		}
+		return nil
+	})
+
+	assert.NoError(t, err)
+}
--- examples/ping/ping_google_with_timeout.star
+++ examples/ping/ping_google_with_timeout.star
@@ -1,7 +1,7 @@
 load("shellout", "exec")
 
 a = exec(
-    name               = "ping google a few times",
+    name               = "ping apple a few times",
     cmd                = "ping", 
     args               = ["-n", "apple.com"],
     timeout            = "6s",
